0d118881c48b4bb4c4a7e48807ff094262a47fba
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-16 17:05:25 +0000

    Completed decoupled version

---
+++ exercises/src/week2_decoupled/HelloWorldDecoupled.java
@@ -0,0 +1,10 @@
+package week2_decoupled;
+
+public class HelloWorldDecoupled {
+	public static void main(String[] args) {
+		StandardOutMessageRenderer mr = new StandardOutMessageRenderer();
+		HelloWorldMessageProvider mp = new HelloWorldMessageProvider();
+		mr.setMessageProvider(mp);
+		mr.render();
+	}
+}
---
+++ exercises/src/week2_decoupled/HelloWorldMessageProvider.java
@@ -0,0 +1,10 @@
+package week2_decoupled;
+
+public class HelloWorldMessageProvider {
+	
+	private String message = "Hello World!";
+
+	public String getMessage() {
+		return message;
+	}
+}
