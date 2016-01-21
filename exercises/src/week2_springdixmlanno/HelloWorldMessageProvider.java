45df2f4b3f7acf6cd2cd9bb80ee7706226bd4a70
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-21 13:40:42 +0000

    Cleaning up the repo

--- exercises/src/week2_springdixmlanno/HelloWorldMessageProvider.java
+++
@@ -1,9 +0,0 @@
-package week2_springdixmlanno;
-
-public class HelloWorldMessageProvider implements MessageProvider {
-   
-	@Override
-    public String getMessage() {
-        return "Hello World!";
-    }
-}
--- exercises/src/week2_springdixmlanno/MessageProvider.java
+++
@@ -1,5 +0,0 @@
-package week2_springdixmlanno;
-
-public interface MessageProvider {
-	String getMessage();
-}
