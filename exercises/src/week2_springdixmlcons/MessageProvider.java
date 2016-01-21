b69f31294a270790394258534d6fde592bf9bed4
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-21 13:37:48 +0000

    Cleaning up the repo

--- exercises/src/week2_springdixmlcons/MessageProvider.java
+++
@@ -1,6 +0,0 @@
-package week2_springdixmlcons;
-
-public interface MessageProvider {
-	
-	String getMessage();
-}
--- exercises/src/week2_springdixmlcons/MessageRenderer.java
+++
@@ -1,9 +0,0 @@
-package week2_springdixmlcons;
-
-public interface MessageRenderer {
-    void render();
-
-    MessageProvider getMessageProvider();
-
-    void setMessageProvider(MessageProvider provider);
-}
