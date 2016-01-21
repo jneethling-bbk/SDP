f0c9fd81d243e34c0cdb0357c82bcd46fea04855
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-21 13:14:33 +0000

    Re-organising the repo

---
+++ exercises/src/week2/decoupled/HelloWorldDecoupled.java
@@ -0,0 +1,10 @@
+package week2.decoupled;
+
+public class HelloWorldDecoupled {
+	public static void main(String[] args) {
+		StandardOutMessageRenderer mr = new StandardOutMessageRenderer();
+		HelloWorldMessageProvider mp = new HelloWorldMessageProvider();
+		mr.setMessageProvider(mp);
+		mr.render();
+	}
+}
---
+++ exercises/src/week2/decoupled/HelloWorldMessageProvider.java
@@ -0,0 +1,10 @@
+package week2.decoupled;
+
+public class HelloWorldMessageProvider {
+	
+	private String message = "Hello World!";
+
+	public String getMessage() {
+		return message;
+	}
+}
