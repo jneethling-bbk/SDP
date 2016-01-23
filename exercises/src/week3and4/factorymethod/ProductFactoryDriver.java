fb51686b65bb650b285871843513f8324fab5d96
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-23 12:55:42 +0000

    Week 3 and 4 fixing factory method

--- exercises/src/week3and4/factorymethod/ProductFactoryDriver.java
+++ exercises/src/week3and4/factorymethod/ProductFactoryDriver.java
@@ -5,7 +5,6 @@ public class ProductFactoryDriver {
 	public static void main(String[] args) {
 		Product p = ConcreteCreator.getInstance().getProduct();
 		System.out.println(p.getString());
-
 	}
 
 }
