77081dfc5c8c80b1d25aa7d95e704de6b60129b8
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-24 15:06:07 +0000

    Week 3 and 4 started with adapter DP exercise

---
+++ exercises/src/week3and4/adapter/XpayImpl.java
@@ -0,0 +1,74 @@
+package week3and4.adapter;
+
+//import lombok.Data;
+
+//@Data
+public class XpayImpl implements Xpay {
+    private String creditCardNo;
+    private String customerName;
+    private String cardExpMonth;
+    private String cardExpYear;
+    private Short cardCVVNo;
+    private Double amount;
+	@Override
+	public String getCreditCardNo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setCreditCardNo(String creditCardNo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String getCustomerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setCustomerName(String customerName) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String getCardExpMonth() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setCardExpMonth(String cardExpMonth) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String getCardExpYear() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setCardExpYear(String cardExpYear) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Short getCardCVVNo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setCardCVVNo(Short cardCVVNo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Double getAmount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setAmount(Double amount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
