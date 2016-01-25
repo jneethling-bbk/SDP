7c2428be4b626628440ef0fbc870fb31592dd71a
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-25 13:09:23 +0000

    Week 3 working on observer DP exercise

--- exercises/src/week3/observer/SMSUsers.java
+++ exercises/src/week3/observer/SMSUsers.java
@@ -1,20 +1,26 @@
 package week3.observer;
 
 public class SMSUsers implements Observer {
-    public SMSUsers(Subject s, String msg){}
+    private Subject s;
+    private String msg;
+	
+	public SMSUsers(Subject s, String msg) {
+		this.s = s;
+		this.msg = msg;
+	}
 
     @Override
     public void update(String desc) {
-
+    	System.out.println(msg + " now getting update: " + desc);
     }
 
     @Override
     public void subscribe() {
-
+    	s.subscribeObserver(this);
     }
 
     @Override
     public void unSubscribe() {
-
+    	s.unSubscribeObserver(this);
     }
 }
