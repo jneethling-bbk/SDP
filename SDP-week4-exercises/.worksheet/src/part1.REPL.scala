d83c64272ca1e59a737b590d2a293c7906188e77
jneethling-bbk <jneethling.bbk@gmail.com>
2016-01-30 17:02:02 +0000

    Started week 4 exercises

---
+++ SDP-week4-exercises/.worksheet/src/part1.REPL.scala
@@ -0,0 +1,7 @@
+package part1
+
+object REPL {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(45); 
+  val myVal = 17;System.out.println("""myVal  : Int = """ + $show(myVal ));$skip(15); 
+  print(myVal)}
+  
+}
---
+++ SDP-week4-exercises/src/part1/REPL.sc
@@ -0,0 +1,7 @@
+package part1
+
+object REPL {
+  val myVal = 17                                  //> myVal  : Int = 17
+  print(myVal)
+  
+}
