c355fc3c334e2914966c6be55c24427a269d2405
jneethling-bbk <jneethling.bbk@gmail.com>
2016-02-03 19:21:47 +0000

    Working on week 4 exercises

--- SDP-week4-exercises/src/part1/Cup.scala
+++ SDP-week4-exercises/src/part1/Cup.scala
@@ -3,11 +3,15 @@ package part1
 class Cup {
   var percentFull = 0
   val max = 100
+  val min = 0
   
   def add(increase:Int):Int = {
     percentFull += increase
     if(percentFull > max) {
       percentFull = max
+    }
+    if (percentFull < min) {
+      percentFull = min
     }
     percentFull // Return this value
   }  
