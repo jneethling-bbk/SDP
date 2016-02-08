719bc73ec29fee48478d378b2f8c0af40a8e4c74
jneethling-bbk <jneethling.bbk@gmail.com>
2016-02-08 10:23:09 +0000

    Week 5 working on facade DP exercises

---
+++ exercises/src/week5/facade/ScheduleServer.java
@@ -0,0 +1,28 @@
+package week5.facade;
+
+public interface ScheduleServer {
+    
+	void startBooting();
+
+    void readSystemConfigFile();
+
+    void init();
+
+    void initializeContext();
+
+    void initializeListeners();
+
+    void createSystemObjects();
+
+    void releaseProcesses();
+
+    void destory();
+
+    void destroySystemObjects();
+
+    void destoryListeners();
+
+    void destoryContext();
+
+    void shutdown();
+}
