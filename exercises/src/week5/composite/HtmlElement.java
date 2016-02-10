e2be08f33f348c8503f360d18c60ac9dad19659a
jneethling-bbk <jneethling.bbk@gmail.com>
2016-02-10 18:18:19 +0000

    Week 5 working on composite DP exercises

---
+++ exercises/src/week5/composite/HtmlElement.java
@@ -0,0 +1,32 @@
+package week5.composite;
+
+public class HtmlElement extends HtmlTag {
+
+	public HtmlElement(String tag) {}
+	
+	@Override
+	public String getTagName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTag(String tag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEndTag(String tag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void generateHtml() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
---
+++ exercises/src/week5/composite/HtmlParentElement.java
@@ -0,0 +1,33 @@
+package week5.composite;
+
+public class HtmlParentElement extends HtmlTag {
+
+	public HtmlParentElement (String tag) {
+		
+	}
+	
+	@Override
+	public String getTagName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTag(String tag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEndTag(String tag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void generateHtml() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
