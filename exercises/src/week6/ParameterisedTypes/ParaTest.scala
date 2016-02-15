b4c0e0d23df2f31405a0d8bd0ba0e7534db6c4cb
jneethling-bbk <jneethling.bbk@gmail.com>
2016-02-15 17:11:00 +0000

    Week 6 completed exercises 20 to 23

--- exercises/src/week6/ParameterisedTypes/ParaTest.scala
+++ exercises/src/week6/ParameterisedTypes/ParaTest.scala
@@ -35,5 +35,23 @@ object ParaTest extends App {
   explicitSet(Vector(10.0, 20.0, 10.0)) is Set(10.0, 20.0)
   explicitSet(Vector(1, 2, 3, 2, 3, 4)) is Set(1.0, 2.0, 3.0, 4.0)
 
+  var str = ""
+  val v = Vector(1, 2, 3, 4)
+  v.foreach(n => str = str + n + ",")
+  
+  str is "1,2,3,4,"
+  
+  val dogYears = (n:Int) => n * 7
+  dogYears(10) is 70
+  
+  var s = ""
+  val v1 = Vector(1, 5, 7, 8)
+  v1.foreach(n => s = s + (n * 7) + " ")
+  s is "7 35 49 56 "
+  
+  var s1 = ""
+  val numbers = Vector(1, 2, 5, 3, 7)
+  numbers.foreach(n => s1 = s1 + (n*n) + " ")
+  s1 is "1 4 25 9 49 "
 
 }
