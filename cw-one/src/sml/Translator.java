91ef50f9d55ae5543db5db7c0a94f366bba8bb9c
jneethling-bbk <jneethling.bbk@gmail.com>
2016-03-04 13:31:52 +0000

    Updated Translator with DivideInstruction handler

--- cw-one/src/sml/Translator.java
+++ cw-one/src/sml/Translator.java
@@ -103,6 +103,11 @@ public class Translator {
             		s1 = scanInt();
             		s2 = scanInt();
             		return new MultiplyInstruction(label, r, s1, s2);
+            	case "div":
+            		r = scanInt();
+            		s1 = scanInt();
+            		s2 = scanInt();
+            		return new DivideInstruction(label, r, s1, s2);
     	   }
 
         // You will have to write code here for the other instructions.
