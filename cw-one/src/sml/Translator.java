94e8317a6361843d3ede01dc8ef8a98a18eb3f58
jneethling-bbk <jneethling.bbk@gmail.com>
2016-03-04 12:44:07 +0000

    Updated Translator with MultiplyInstruction handler

--- cw-one/src/sml/Translator.java
+++ cw-one/src/sml/Translator.java
@@ -98,6 +98,11 @@ public class Translator {
             		s1 = scanInt();
             		s2 = scanInt();
             		return new SubtractInstruction(label, r, s1, s2);
+            	case "mul":
+            		r = scanInt();
+            		s1 = scanInt();
+            		s2 = scanInt();
+            		return new MultiplyInstruction(label, r, s1, s2);
     	   }
 
         // You will have to write code here for the other instructions.
