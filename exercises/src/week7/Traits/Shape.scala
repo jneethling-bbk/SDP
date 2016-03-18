12a154a08a78d2217a3b780b5e0ce1f7f86bca84
jneethling-bbk <jneethling.bbk@gmail.com>
2016-03-18 17:01:19 +0000

    Completed Q5b of week 7 Scala exercises

--- exercises/src/week7/Traits/Shape.scala
+++ exercises/src/week7/Traits/Shape.scala
@@ -12,14 +12,18 @@ case class Circle(radius : Double) extends Shape {
   val area = math.Pi * radius * radius
 }
 
-case class Rectangle(height : Double, width : Double) extends Shape {
+trait RectangularShape extends Shape {
+  def height: Int
+  def width: Int
   val sides = 4
   val perimeter = (height * 2) + (width * 2)
   val area = height * width
 }
 
-case class Square(height : Double) extends Shape {
-  val sides = 4
-  val perimeter = height * 4
-  val area = height * height
+case class Rectangle(height : Double, width : Double) extends RectangularShape
+
+case class Square(sideLength : Double) extends RectangularShape {
+  val height = sideLength
+  val width = sideLength
+  
 }
